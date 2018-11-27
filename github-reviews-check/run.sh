f0c600cd8ee72332425d58e095e628198319bec1
Calvin Rodo <calvin.rodo@hrsdc-rhdcc.gc.ca>
2018-11-27 08:49:59 -0500

    Refactor

---
+++ github-reviews-check/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+export ORIGIN='cdssnc/github-reviews-check-compliance:latest'
+export COMPONENT='Infrastructure'
+export DESCRIPTION='The GitHub repo shows pull requests with reviews.'
+export SATISFIES='SA-11 (4)'
+export REPO_URL='https://github.com/cds-snc/vac-benefits-directory'
+./main
